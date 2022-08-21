914dec90bfdb10405b3d0791576fe256ae669532
Meir Halachmi <meir.halachmi@gmail.com>
2022-08-21 16:57:27 +0300

    Updates

---
+++ precache-manifest.0cb3065edc2bd51d25098a5105eb97ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ba6d16f728cb7befc4c4b74a165298",
+    "url": "/Tests-Scheduler/index.html"
+  },
+  {
+    "revision": "bcb5b32bd5288967aca3",
+    "url": "/Tests-Scheduler/static/css/2.193a939a.chunk.css"
+  },
+  {
+    "revision": "59d4cee15332652d4439",
+    "url": "/Tests-Scheduler/static/css/main.ce9cdfa0.chunk.css"
+  },
+  {
+    "revision": "bcb5b32bd5288967aca3",
+    "url": "/Tests-Scheduler/static/js/2.19e27fd0.chunk.js"
+  },
+  {
+    "revision": "59d4cee15332652d4439",
+    "url": "/Tests-Scheduler/static/js/main.fd182881.chunk.js"
+  },
+  {
+    "revision": "8cebb96f8ab2b597600b",
+    "url": "/Tests-Scheduler/static/js/runtime~main.438457be.js"
+  }
+]);
